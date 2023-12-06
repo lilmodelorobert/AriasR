75fd73e00d2ca57d792babab75360a13b830d1ae
lilmodelorobert <robertarias050613@gmail.com>
2023-12-06 14:43:27 -0800

    daily forecast

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,30 @@
+const apiKey = "d7aa370d43983f7ec8a73ae3b04a9057";
+let clientSearch = document.getElementById("clientSearch");
+let displayCity = document.getElementById("displayCity");
+let displayTemp = document.getElementById("displayTemp");
+let enterKey;
+let  High;
+let Low;
+// let High1;
+// let Low1;
+// let High2;
+// let Low2;
+// let Low3;
+// let High3
+// let Low4
+// let High4
+
+
+
+
+
+
+
+
+
+
+
+
 
 //geolocation
 navigator.geolocation.getCurrentPosition(success, errorFunc);
@@ -10,31 +37,55 @@ function errorFunc(error) {
     console.log(error.message);
 }
 //end of geo location
-let cityname
-const apiKey = "d7aa370d43983f7ec8a73ae3b04a9057";
-apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=${apiKey}&units=imperial"
 
-let searchInput = document.getElementById("searchInput")
-let enterKey
-async function searchWeather(city) {
-    try {
-        const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
-        console.log(response);
+let cityName;
 
-        if (!response.ok) {
-            throw new Error('Network was not ok');
-        }
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        console.error('error fetching weather data:', error);
-    }
+async function searchWeather() {
+    cityName = clientSearch.value.toLowerCase();
+    const promise = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=imperial`);
+    const data = await promise.json();
+    console.log(data);
+    
 }
-enterKey.addeventlistener("keypress", function(event){
-    if (event.key== "enter"){
-        event.preventDefault ();
-        searchWeather()
-    }
-})
+searchBtn.addEventListener("click", function (event) {
+        searchWeather();
+        fiveDay();
+}
+);
+async function fiveDay(){
+    cityName = clientSearch.value.toLowerCase();
+    const promise= await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}&units=imperial`);
+    const data = await promise.json();
+    
+    console.log(data.list[0].main.temp_max, data.list[0].main.temp_min);
+    High = Math.round(data.list[0].main.temp_max);
+    Low = Math.round(data.list[0].main.temp_min);
+    hnl0.innerText = `↑${High}° ↓${Low}°`;
+
+
+    console.log(data.list[8].main.temp_max, data.list[0].main.temp_min);
+    High = Math.round(data.list[8].main.temp_max);
+    Low = Math.round(data.list[8].main.temp_min);
+    hnl1.innerText = `↑${High}° ↓${Low}°`;
+
+
+    console.log(data.list[16].main.temp_max, data.list[0].main.temp_min);
+    High = Math.round(data.list[16].main.temp_max);
+    Low = Math.round(data.list[16].main.temp_min);
+    hnl2.innerText = `↑${High}° ↓${Low}°`;
+
+
+    console.log(data.list[24].main.temp_max, data.list[0].main.temp_min);
+    High = Math.round(data.list[24].main.temp_max);
+    Low = Math.round(data.list[24].main.temp_min);
+    hnl3.innerText = `↑${High}° ↓${Low}°`;
+
+    console.log(data.list[32].main.temp_max, data.list[0].main.temp_min);
+    High = Math.round(data.list[32].main.temp_max);
+    Low = Math.round(data.list[32].main.temp_min);
+    hnl4.innerText = `↑${High}° ↓${Low}°`;
+
+}
+
 
 
